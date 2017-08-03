a3d64df441da0e284ddd7032fbc3815dfac62a8a
Dan Alloway <dan@micahsix.com>
2017-08-03 15:50:52 -0400

    memoize result

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
-module.exports = function() {
-    var pixelRatio = window.devicePixelRatio || 1;
-    var offset = 12 * pixelRatio;
-    var node = window.document.createElement('canvas');
+const detect = () => {
+    if (detect.status === null) {
+        var pixelRatio = window.devicePixelRatio || 1;
+        var offset = 12 * pixelRatio;
+        var node = window.document.createElement('canvas');
 
-    var ctx = node.getContext('2d');
-    ctx.fillStyle = '#f00';
-    ctx.textBaseline = 'top';
-    ctx.font = '32px Arial';
-    ctx.fillText('\ud83d\udc28', 0, 0); // U+1F428 KOALA
+        var ctx = node.getContext('2d');
+        ctx.fillStyle = '#f00';
+        ctx.textBaseline = 'top';
+        ctx.font = '32px Arial';
+        ctx.fillText('\ud83d\udc28', 0, 0); // U+1F428 KOALA
 
-    return ctx.getImageData(offset, offset, 1, 1).data[0] !== 0;
+        detect.status = ctx.getImageData(offset, offset, 1, 1).data[0] !== 0;
+    }
+
+    return detect.status;
 };
+
+detect.status = null;
+
+module.exports = detect;
